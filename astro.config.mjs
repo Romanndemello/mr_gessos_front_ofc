026052ffe8f4141d41632020c711c3bcec0d56e2
Romann <zillymango@gmail.com>
2024-02-26 16:32:34 -0300

    alteracao href formulario

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from "astro-icon"
-
+import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false,
